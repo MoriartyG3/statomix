ad4e51dddcc08d6fd4e495a0d7fedcf5e190d9be
Saahil <saahilw26@gmail.com>
2026-08-05 13:57:18 +0530

    Development

---
+++ statomix/__init__.py
@@ -0,0 +1,10 @@
+"""Statomix: tools for dataset cleaning and statistical analysis."""
+
+from . import analytics, dataset, pipelines, project
+
+__all__ = [
+    "analytics",
+    "dataset",
+    "pipelines",
+    "project",
+]
